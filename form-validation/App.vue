1e42b540f6724fcea86eafd6204c315c4b260176
Andrei Chirkov <aachirkov@me.com>
2022-05-12 22:49:40 +0500

    first commit

---
+++ form-validation/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <my-button
+      background="darkslateblue"
+      color="white"
+      :disabled="!valid"
+  />
+</template>
+
+<script>
+import MyButton from "./MyButton.vue";
+export default {
+  name: "App",
+  components: {
+    MyButton
+  },
+  data() {
+    return {
+      valid: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+button:disabled {
+  opacity: 0.5;
+}
+
+button {
+  background: none;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 40px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  filter: brightness(125%);
+}
+</style>
---
+++ form-validation/MyButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    :style="{ background, color }"
+    :disabled="disabled"
+  >
+    Button
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  props: {
+    background: {
+      type: String
+    },
+    color: {
+      type: String
+    },
+    disabled: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

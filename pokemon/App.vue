f1101fd7164cd332e6cdc6dbfaeb835f760c750e
Andrei Chirkov <aachirkov@me.com>
2022-06-08 17:09:05 +0500

    Add fetching pokemon with array of data

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -8,19 +8,34 @@
 
 <script>
 const api = 'https://pokeapi.co/api/v2/pokemon'
+const ids = [1, 4, 7]
 
 export default {
   name: "App",
   data() {
     return {
-      pokemon: null
+      pokemon: []
     }
   },
   methods: {
     async fetchData() {
-      const res = await fetch(`${api}/1`)
-      const json = await res.json()
+      const responses = await Promise.all(
+          ids.map(id => fetch(`${api}/${id}`))
+      )
+
+      const json = await Promise.all(
+          responses.map(data => data.json())
+      )
       console.log(json)
+
+      this.pokemon = json.map(datum => ({
+        id: datum.id,
+        name: datum.name,
+        sprite: datum.sprites.other['official-artwork'].front_default,
+        types: datum.types.map(type => type.type.name)
+      }))
+      // const json = await res.json()
+      // console.log(json)
       this.pokemon = {
         id: json.id,
         name: json.name,

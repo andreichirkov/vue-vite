f4096f1178f8091c3341cabe602da33b7ee995cd
Andrei Chirkov <aachirkov@me.com>
2022-06-08 16:47:09 +0500

    Add fetching pokemon with data

---
+++ pokemon/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card">
+    <div class="title">Title</div>
+    <div class="content">Content</div>
+    <div @click="fetchData" class="description">Description</div>
+  </div>
+</template>
+
+<script>
+const api = 'https://pokeapi.co/api/v2/pokemon'
+
+export default {
+  name: "App",
+  data() {
+    return {
+      pokemon: null
+    }
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch(`${api}/1`)
+      const json = await res.json()
+      console.log(json)
+      this.pokemon = {
+        id: json.id,
+        name: json.name,
+        sprite: json.sprites.other['official-artwork'].front_default,
+        types: json.types.map(type => type.type.name)
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid silver;
+  border-radius: 8px;
+  max-width: 200px;
+  margin: 0 5px;
+  cursor: pointer;
+  box-shadow: 0px 1px 3px darkgrey;
+  transition: 0.2s;
+}
+.title, .content, .description {
+  padding: 16px;
+  text-transform: capitalize;
+  text-align: center;
+}
+.title, .content {
+  border-bottom: 1px solid silver;
+}
+.title {
+  font-size: 1.25em;
+}
+.card:hover {
+  transition: 0.2s;
+  box-shadow: 0px 1px 9px darkgrey;
+}
+</style>

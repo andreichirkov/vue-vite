c40b7ed8b1e4c2282e29790f3ddc1c2ecca2bb71
Andrei Chirkov <aachirkov@me.com>
2022-06-09 12:16:19 +0500

    Reusable comp card

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="cards">
-    <div class="card" v-for="p in pokemon" :key="p.id">
-      <div class="title">
-        {{ p.name }}
-      </div>
-      <div class="content">
-        <img :src="p.sprite" alt="img">
-      </div>
-      <div class="description" v-for="type in p.types" :key="type">
-        {{type}}
-      </div>
-    </div>
+    <card
+      v-for="pokemon in pokemons"
+      :key="pokemon.id"
+      :pokemon="pokemon"/>
   </div>
 </template>
 
 <script>
+import Card from "./Card.vue";
+
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const ids = [1, 4, 7]
 
 export default {
   name: "App",
+  components: {
+    Card
+  },
   data() {
     return {
-      pokemon: []
+      pokemons: []
     }
   },
   created() {
@@ -38,7 +36,7 @@ export default {
           responses.map(data => data.json())
       )
 
-      this.pokemon = json.map(datum => ({
+      this.pokemons = json.map(datum => ({
         id: datum.id,
         name: datum.name,
         sprite: datum.sprites.other['official-artwork'].front_default,
@@ -55,36 +53,5 @@ export default {
   display: flex;
 }
 
-img {
-  width: 100%;
-}
 
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-
-.title, .content, .description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-
-.title, .content {
-  border-bottom: 1px solid silver;
-}
-
-.title {
-  font-size: 1.25em;
-}
-
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgrey;
-}
 </style>
---
+++ pokemon/Card.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="card">
+    <div class="title">
+      {{ pokemon.name }}
+    </div>
+    <div class="content">
+      <img :src="pokemon.sprite" alt="img">
+    </div>
+    <div class="description" v-for="type in pokemon.types" :key="type">
+      {{ type }}
+    </div>
+  </div>
+</template>
+
+<script>
+const api = 'https://pokeapi.co/api/v2/pokemon'
+const ids = [1, 4, 7]
+
+export default {
+  name: "Card",
+  props: {
+    pokemon: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+
+}
+
+</script>
+
+<style scoped>
+
+img {
+  width: 100%;
+}
+
+.card {
+  border: 1px solid silver;
+  border-radius: 8px;
+  max-width: 200px;
+  margin: 0 5px;
+  cursor: pointer;
+  box-shadow: 0px 1px 3px darkgrey;
+  transition: 0.2s;
+}
+
+.title, .content, .description {
+  padding: 16px;
+  text-transform: capitalize;
+  text-align: center;
+}
+
+.title, .content {
+  border-bottom: 1px solid silver;
+}
+
+.title {
+  font-size: 1.25em;
+}
+
+.card:hover {
+  transition: 0.2s;
+  box-shadow: 0px 1px 9px darkgrey;
+}
+</style>

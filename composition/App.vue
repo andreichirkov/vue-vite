9ce9f4afcc6059fa65d5ac75b96cb41b73229e91
Andrei Chirkov <aachirkov@me.com>
2022-06-12 17:16:36 +0500

    Ref and reactive

---
+++ composition/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+  <button @click="increase('a')">{{ numbers.a }}</button>
+  <button @click="increase('b')">{{ numbers.b }}</button>
+</template>
+
+<script>
+import {ref, reactive} from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    const numbers = reactive({
+      a: 0,
+      b: 0
+    })
+
+    const increment = () => {
+      count.value++
+    }
+
+    const increase = (n) => {
+      numbers[n]++
+    }
+
+    return {
+      count,
+      increment,
+      numbers,
+      increase
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+button {
+  height: 100px;
+  width: 100px;
+  font-size: 40px;
+}
+</style>
+

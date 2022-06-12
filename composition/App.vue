14b2912bfdf1f82254b0f0067fa0faa6d8bebf68
Andrei Chirkov <aachirkov@me.com>
2022-06-12 17:25:23 +0500

    Computed

--- composition/App.vue
+++ composition/App.vue
@@ -2,17 +2,18 @@
   <button @click="increment">{{ count }}</button>
   <button @click="increase('a')">{{ numbers.a }}</button>
   <button @click="increase('b')">{{ numbers.b }}</button>
+  <p>{{total}}</p>
 </template>
 
 <script>
-import {ref, reactive} from 'vue'
+import {ref, reactive, computed} from 'vue'
 
 export default {
   setup() {
     const count = ref(0)
     const numbers = reactive({
-      a: 0,
-      b: 0
+      a: 1,
+      b: 2
     })
 
     const increment = () => {
@@ -23,11 +24,16 @@ export default {
       numbers[n]++
     }
 
+    const total = computed(() => {
+      return count.value + numbers.a + numbers.b
+    })
+
     return {
       count,
       increment,
       numbers,
-      increase
+      increase,
+      total
     }
   }
 }
@@ -35,6 +41,10 @@ export default {
 </script>
 
 <style scoped>
+p {
+  font-size: 40px;
+}
+
 button {
   height: 100px;
   width: 100px;
